0c5393549ef7343d76608b370686dae1c7952eb4
vatari <majestic13@gmail.com>
2023-04-20 15:19:10 +0300

    added fetch request

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   if (loading) {
     return (
       <main>
